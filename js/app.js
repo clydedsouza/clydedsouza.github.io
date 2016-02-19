c7e6b4afbf7fe6e688fae7d0ab3e47ace91f781c
Clyde D'Souza <clyd94@gmail.com>
2016-02-20 00:10:26 +1300

    Color scheme fixed, components added

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 ﻿angular
 .module('clyde', ['ngMaterial', 'ngRoute', 'ngAnimate'])
-.config(['$routeProvider', function ($routeProvider) {
+.config(['$routeProvider', '$mdThemingProvider', function ($routeProvider, $mdThemingProvider) {
     $routeProvider
-    .when('/', { templateUrl: "views/bout.html", controller:"AboutController", title:"Clyde D'Souza", tabIndex:0 })
-    .when('/projects', { templateUrl: "views/projects.html", controller: "ProjectsController", title: "Projects | Clyde D'Souza", tabIndex:1 })
-    .otherwise({ templateUrl: "views/not-found.html" });
+        .when('/', { templateUrl: "views/bout.html", controller:"AboutController", title:"Clyde D'Souza", tabIndex:0 })
+        .when('/projects', { templateUrl: "views/projects.html", controller: "ProjectsController", title: "Projects | Clyde D'Souza", tabIndex:1 })
+        .otherwise({ templateUrl: "views/not-found.html" });
+    $mdThemingProvider.theme('default')
+        .primaryPalette('teal')
+        .accentPalette('yellow');
 }])
 .run(['$location', '$rootScope', function ($location, $rootScope) {
     $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
@@ -47,6 +50,20 @@
     $scope.onSwipeRight = function (ev) {
         if (ev == "projects") {
             $window.location = "#/";
+        }
+    };
+    $scope.share = function (x) {
+        if (x == "tweet") {
+            $window.open("https://twitter.com/share?text=Amazing%20website!%20Have%20a%20look%20at%20@ClydeDz%20's%20website%20at%20&url=http://clydedsouza.github.io", "_blank");
+        }
+        else if (x == "fb") {
+            $window.open("http://www.facebook.com/sharer.php?u=http://clydedsouza.github.io", "_blank");
+        }
+        else if (x == "google") {
+            $window.open("https://plus.google.com/share?url=http://clydedsouza.github.io", "_blank");
+        }
+        else {
+            $window.open("http://www.linkedin.com/shareArticle?mini=true&amp;url=http://clydedsouza.github.io", "_blank");
         }
     };
     $scope.topDirections = ['left', 'up'];
