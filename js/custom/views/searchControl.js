3d29d5a498143419a54851a5f0fd01b81b5d8bf2
Clyde D'Souza <clyd94@gmail.com>
2019-06-18 23:46:02 +1200

    Add multiselect control to the search control

--- js/custom/views/searchControl.js
+++ js/custom/views/searchControl.js
@@ -10,6 +10,16 @@ function searchControlViewPreSwitchTemplate() {
 
 function initSearchControls() {
     var searchControlParentView = $("#display nav a.active").attr('data-partialview');
+    var data = []; 
+    for (var i = 0; i < 100; i++) {
+        data.push({
+            label: 'Option ' + (i + 1) ,
+            title: 'Option ' + (i + 1) ,
+            value: i + '-'
+        });
+    }
+    $('#example-getting-started').multiselect('dataprovider', data);
+    //$('#example-getting-started').multiselect();
 
     $("#searchBtn").on("click", function () {
         var filter = { searchText: $("#searchTxt").val() };
