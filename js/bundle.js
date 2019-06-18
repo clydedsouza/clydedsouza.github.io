3ed3446384a3253e7d08a79dc32e4c2ef4a09ccc
Clyde D'Souza <clyd94@gmail.com>
2019-06-18 22:29:13 +1200

    Add keyup event to search control

--- js/bundle.js
+++ js/bundle.js
@@ -708,6 +708,14 @@ function initSearchControls() {
         var filter = { searchText: $("#searchTxt").val() };
         filterAndDisplayProjectItems(filter);
     });
+    $("#searchTxt").on("keyup", function () {
+        var searchInput = $("#searchTxt").val();
+        if (searchInput.length > 0 && searchInput.length < 3) {
+            return;
+        }
+        var filter = { searchText: searchInput };
+        filterAndDisplayProjectItems(filter);
+    });
 }
 
 var templates = {
--- js/custom/views/searchControl.js
+++ js/custom/views/searchControl.js
@@ -13,4 +13,12 @@ function initSearchControls() {
         var filter = { searchText: $("#searchTxt").val() };
         filterAndDisplayProjectItems(filter);
     });
+    $("#searchTxt").on("keyup", function () {
+        var searchInput = $("#searchTxt").val();
+        if (searchInput.length > 0 && searchInput.length < 3) {
+            return;
+        }
+        var filter = { searchText: searchInput };
+        filterAndDisplayProjectItems(filter);
+    });
 }
