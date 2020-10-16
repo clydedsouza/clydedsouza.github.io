e72359e5b88c1e1b7ee00da18ec1ca6d25806f91
Clyde D'Souza <clyd94@gmail.com>
2020-10-16 20:03:00 +1300

    Remove tech and category dropdown

--- js/custom/views/searchControl.js
+++ js/custom/views/searchControl.js
@@ -2,24 +2,12 @@
     initSearchControls(); 
 }
 
-
 function searchControlViewPreSwitchTemplate() {
     switchTemplate("searchControlPartial", {});
 }
 
-
 function initSearchControls() {
     var searchControlParentView = $("nav a.active").attr('data-partialview');
-
-    initMultiselect(searchControlParentView);
-
-    $("#searchBtn").on("click", function () { 
-        var filter = {
-            searchText: $("#searchTxt").val(),
-            showInactive: $("#includeInactive").is(":checked")
-        };
-        filterAndDisplayProjectItems(filter, searchControlParentView);
-    });
 
     $("#searchTxt").on("keyup", function () {
         var searchInput = $("#searchTxt").val(); 
@@ -39,101 +27,6 @@ function initSearchControls() {
         }; 
         filterAndDisplayProjectItems(filter, searchControlParentView); 
     });
-}
-
-function initMultiselect(key) {
-    var projectItemsData = getLocalData(key);
-    if (projectItemsData === "" || projectItemsData === null) {
-        $('#techMultiselect').hide();
-        $('#categoriesMultiselect').hide();
-        return;
-    }
-
-    var multiselectData = {categories : [], madeUsing: []};
-
-    for (var i = 0; i < projectItemsData.projects.length; i++) {
-
-        if (key === "projectsPartial") {
-            for (var j = 0; j < projectItemsData.projects[i].madeUsing.length; j++) {
-                multiselectData.madeUsing.push(projectItemsData.projects[i].madeUsing[j]); 
-            } 
-        }
-
-        multiselectData.categories.push(projectItemsData.projects[i].category);
-        
-    } 
-
-    var refinedMultiselectData = { categories: [], madeUsing: [] };
-    var searchControlParentView = $("nav a.active").attr('data-partialview');
-
-    $.each(multiselectData.categories, function (i, el) { 
-        var targetData = refinedMultiselectData.categories.map(function (a) { return a.label; }); 
-        if ($.inArray(el, targetData) === -1) {
-            refinedMultiselectData.categories.push({
-                label: el, title: el, value: el
-            }); 
-        }
-    });     
-
-    if (key === "projectsPartial") {
-        $.each(multiselectData.madeUsing, function (i, el) {
-            var targetData = refinedMultiselectData.madeUsing.map(function (a) { return a.label; });
-            if ($.inArray(el, targetData) === -1) {
-                refinedMultiselectData.madeUsing.push({
-                    label: el, title: el, value: el
-                });
-            }
-        });
-        $('#techMultiselect').multiselect({
-            enableFiltering: true,
-            maxHeight: 200,
-            nonSelectedText: 'Filter by tech',
-            enableCaseInsensitiveFiltering: true,
-            templates: {
-                filter: '<li class="multiselect-item multiselect-filter"><div class="input-group"><span class="input-group-addon"><i class="fas fa-search"></i></span><input class="form-control multiselect-search" type="text"></div></li>',
-                filterClearBtn: '<span class="input-group-btn"><button class="btn btn-default multiselect-clear-filter" type="button"><i class="fas fa-times"></i></button></span>'
-            },
-            onChange: function (element, checked) {
-                var filter = {
-                    searchText: $("#searchTxt").val(),
-                    showInactive: $("#techMultiselect").is(":checked"),
-                    techOptions: getSearchMultiselectOptions($('#techMultiselect option:selected')),
-                    categoryOptions: getSearchMultiselectOptions($('#categoriesMultiselect option:selected'))
-                }; 
-                filterAndDisplayProjectItems(filter, searchControlParentView);
-            }
-        });
-        $('#techMultiselect').multiselect('dataprovider', refinedMultiselectData.madeUsing);
-    }
-    else {
-        $('#techMultiselect').hide();
-    }
-
-    if (key === "speakingPartial" || key === "teachingPartial") {
-        $("#includeInactiveContainer").hide();
-    }
-
-
-    $('#categoriesMultiselect').multiselect({
-        enableFiltering: true,
-        maxHeight: 200,
-        nonSelectedText: 'Filter by category',
-        enableCaseInsensitiveFiltering: true,
-        templates: {
-            filter: '<li class="multiselect-item multiselect-filter"><div class="input-group"><span class="input-group-addon"><i class="fas fa-search"></i></span><input class="form-control multiselect-search" type="text"></div></li>',
-            filterClearBtn: '<span class="input-group-btn"><button class="btn btn-default multiselect-clear-filter" type="button"><i class="fas fa-times"></i></button></span>'
-        },
-        onChange: function (element, checked) {
-            var filter = {
-                searchText: $("#searchTxt").val(),
-                showInactive: $("#techMultiselect").is(":checked"),
-                techOptions: getSearchMultiselectOptions($('#techMultiselect option:selected')),
-                categoryOptions: getSearchMultiselectOptions($('#categoriesMultiselect option:selected'))
-            };
-            filterAndDisplayProjectItems(filter, searchControlParentView);
-        }
-    });
-    $('#categoriesMultiselect').multiselect('dataprovider', refinedMultiselectData.categories);
 }
 
 function getSearchMultiselectOptions(searchMultiselectSelectedOptions) {
