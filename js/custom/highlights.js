6b96f939e95fb842d23e5036c8af577c399bf5af
Clyde D'Souza <clyd94@gmail.com>
2020-10-17 00:07:58 +1300

    Update max limit for random calc

--- js/custom/highlights.js
+++ js/custom/highlights.js
@@ -1,7 +1,7 @@
 ﻿function loadRandomHighlight() {
     $.get("https://api.clydedsouza.net/allhighlights.json", function (data) {
         var jsonData = data.fileMap;
-        var randomSelection = Math.floor(Math.random() * (Object.keys(jsonData).length + 1));
+        var randomSelection = Math.floor(Math.random() * Object.keys(jsonData).length);
         var highlightData = jsonData[Object.keys(jsonData)[randomSelection]];
         $(".footer-highlights a, .sidebar-highlights a")
             .attr("href", highlightData.website)
