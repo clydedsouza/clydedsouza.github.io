3d0ff5af05d9110f8a2b11bf7a35123e70162a0b
Clyde D'Souza <clyd94@gmail.com>
2020-10-18 13:06:55 +1300

    Move cookie logic to its own file

--- js/bundle.js
+++ js/bundle.js
@@ -6539,11 +6539,6 @@ $(document).ready(function () {
         templates[$(this).attr('data-partialview')].preSwitchTemplate(); 
     }); 
 
-    $(".cookie-notice a:first-child").on('click', function () {
-        document.cookie = "cookie-accepted=true";
-        $(".cookie-notice").hide();
-    }); 
-
     var browserURL = "";
     $(window).on('popstate', function (event) {
         // This handles the browser back and forward button.
@@ -6573,27 +6568,6 @@ $(document).ready(function () {
     }, 1500);
 });
 
-function showCookieNotice() {
-    var cookieAccepted = getCookie("cookie-accepted");
-    if (!cookieAccepted) return false;
-    $(".cookie-notice").hide();
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
 function getCacheInvalidationDate() {
     return "?v=" + appVersion;
 }
@@ -6825,3 +6799,32 @@ function loadRandomHighlight() {
             .text(highlightData.text);        
     });  
 }
+
+function showCookieNotice() {
+    var cookieAccepted = getCookie("cookie-accepted");
+    if (!cookieAccepted) return false;
+    $(".cookie-notice").hide();
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+$(document).ready(function () {
+    $(".cookie-notice a:first-child").on('click', function () {
+        document.cookie = "cookie-accepted=true";
+        $(".cookie-notice").hide();
+    }); 
+});
