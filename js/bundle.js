41ce688e3d0e8373db9663fefa67caba1339f65a
clydedz <clyd94@gmail.com>
2022-02-19 21:32:15 +1300

    Remove cookie usage notice

--- js/bundle.js
+++ js/bundle.js
@@ -4755,7 +4755,6 @@ var appVersion = 0.1;
 $(document).ready(function () {
     manageWebsiteRouting(window.location.href);
     loadRandomHighlight();
-    showCookieNotice();
 
     $("nav a").on('click', function () {  
         activateNavigationMenuItem($(this).attr('data-partialview'));
@@ -5022,32 +5021,3 @@ function loadRandomHighlight() {
             .text(highlightData.text);        
     });  
 }
-
-function showCookieNotice() {
-    var cookieAccepted = getCookie("cookie-accepted");
-    if (!cookieAccepted) return false;
-    $(".cookie-notice").hide();
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-$(document).ready(function () {
-    $(".cookie-notice a:first-child").on('click', function () {
-        document.cookie = "cookie-accepted=true";
-        $(".cookie-notice").hide();
-    }); 
-});
